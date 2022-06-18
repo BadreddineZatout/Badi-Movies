58a549f10b0d91e3e56d7483968b947e76696ccf
BadreddineZatout <hb_zatout@esi.dz>
2022-06-18 22:21:09 +0100

    responsive nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,8 @@
 <template>
   <nav
     class="
-      flex
-      items-center
-      justify-between
-      flex-wrap
-      bg-MediumCarmine
+      sm:flex sm:items-center sm:justify-between sm:flex-wrap
+      bg-orange-600
       py-1
       lg:px-12
       shadow
@@ -13,8 +10,37 @@
   >
     <div class="flex items-center justify-start gap-4">
       <div class="flex items-center">
+        <div class="sm:hidden ml-3">
+          <button
+            class="block h-8 mr-3 text-white items-center hover:font-bold"
+            @click="show = !show"
+          >
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                x-show="!show"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+              <path
+                x-show="show"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
         <svg
-          class="w-16 h-16 text-white"
+          class="w-12 h-12 sm:w-16 sm:h-16 text-white"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
@@ -27,9 +53,13 @@
             d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
           ></path>
         </svg>
-        <h1 class="text-white text-2xl font-bold ml-2">Badi Movie</h1>
+        <h1 class="text-white text-lg ml-2 sm:text-2xl sm:font-bold">
+          Badi Movie
+        </h1>
       </div>
-      <div class="flex items-centre text-white">
+
+      <!-- Web View -->
+      <div class="hidden sm:flex items-centre text-white">
         <router-link :to="{ name: 'Home' }">
           <button
             class="hover:font-semibold p-2 mr-2"
@@ -64,7 +94,7 @@
         </router-link>
       </div>
     </div>
-    <div class="w-1/4 flex items-center justify-end">
+    <div class="hidden sm:flex items-center justify-end w-1/4">
       <input
         type="text"
         placeholder="Search for movie"
@@ -85,12 +115,59 @@
         ></path>
       </svg>
     </div>
+    <!-- Mobile View -->
+    <div
+      v-show="show"
+      class="
+        sm:hidden
+        grid grid-cols-1
+        gap-y-2
+        pl-2
+        items-centre
+        text-white text-left
+      "
+    >
+      <router-link :to="{ name: 'Home' }">
+        <button
+          class="hover:font-semibold p-2 mr-2"
+          :class="{ 'font-bold': route.fullPath === '/' }"
+        >
+          Movies
+        </button>
+      </router-link>
+      <router-link :to="{ name: 'Actors' }">
+        <button
+          class="hover:font-semibold p-2 mr-2"
+          :class="{ 'font-bold': route.fullPath === '/actors' }"
+        >
+          Actors
+        </button>
+      </router-link>
+      <router-link :to="{ name: 'Profile' }">
+        <button
+          class="hover:font-semibold p-2 mr-2"
+          :class="{ 'font-bold': route.fullPath === '/profile' }"
+        >
+          Profile
+        </button>
+      </router-link>
+      <router-link :to="{ name: 'Contact' }">
+        <button
+          class="hover:font-semibold p-2 mr-2"
+          :class="{ 'font-bold': route.fullPath === '/contact' }"
+        >
+          Contact
+        </button>
+      </router-link>
+    </div>
   </nav>
 </template>
 
 <script setup>
 import { useRoute } from "vue-router";
+import { ref } from "vue";
 const route = useRoute();
+let show = ref(false);
 </script>
 
 <style lang="scss" scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        DarkChestnut: "#977563",
-        MediumCarmine: "#A6492B",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
