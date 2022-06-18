38163815c8961298564f72e374221848f0f71132
BadreddineZatout <hb_zatout@esi.dz>
2022-06-18 19:05:25 +0100

    init tailwindcss, vue router & vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./index.css";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import router from "./routes";
+import store from "./store";
+
+createApp(App).use(router).use(store).mount("#app");
